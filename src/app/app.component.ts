a5c14ec70ec9b1b027b897616d1be19b89b703ac
BasamDawii <basa0033@easv365.dk>
2024-02-02 09:07:45 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Frontend';
+  messeges: string[] = [];
+  ws: WebSocket = new WebSocket("ws://localhost:8181");
+  messageContent = new FormControl('');
+
+
+  constructor() {
+    this.ws.onmessage = message =>{
+      this.messeges.push(message.data)
+    }
+  }
+
+  sendMessage() {
+    this.ws.send(this.messageContent.value!);
+  }
 }
